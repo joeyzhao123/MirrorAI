792eb70f5d61c733c831f9bdb5387ba4863df334
Stephen Yang <stevyk6@gmail.com>
2021-03-04 23:04:37 -0800

    added things to do to build the chess

---
+++ game.py
@@ -0,0 +1,17 @@
+import pygame as py
+
+# TODO
+# 1. Make Board
+# 2. Make Chess Pieces
+#   a) pawns
+#   b) knights
+#   c) bishops
+#   d) rooks
+#   e) queen
+#   f) king
+# 3. Make Game Logic
+#   a) Check
+#   b) pins
+#   c) Castling
+#   d) Stalemate
+#   e) En passant
